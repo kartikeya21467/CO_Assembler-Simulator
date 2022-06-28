a08f61692322da923056b6f652bb86e25d1a4410
kartikeya21467 <kartikeya21467@iiitd.ac.in>
2022-06-28 18:34:02 +0530

    28JuneRelease

--- assembly.py
+++ assembly.py
@@ -1,11 +1,17 @@
 import sys
 
+reg_bin={'R0':'000','R1':'001','R2':'010','R3':'011','R4':'100','R5':'101',
+        'R6':'110','FLAGS':'111'}
+
+
+
 assemb_inst=sys.stdin.read()
 
 lines=assemb_inst.split()
 for i in lines:
     
     inst=i.split()
-    if(inst[-1]=='R0'):
-        print("Yes")
     
+    for j in inst:
+        print(j)
+
